7025ecf1193ca8f1972ef88da1e88a5b85585b31
pinguwu <42652753+pinguwu@users.noreply.github.com>
2019-09-17 13:54:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready( function () {
   var Ball = function () {
     this.x = 50;
     this.y = 300;
+    this.changeInY = 0;
     
     this.draw = function (thisX, thatY) {
       ctx.strokeStyle = "#000000";
@@ -14,12 +15,26 @@ $(document).ready( function () {
       ctx.arc(thisX, thatY, 20, 0, Math.PI * 2, false);
       ctx.stroke();
     }
+    
+    this.jump = function () {
+      this.changeInY = 10
+      this.y = this.y - this.changeInY;
+      while (this.y <= 300) {
+        this.changeInY = this.changeInY - 1;
+        this.y = this.y - this.changeInY;
+      }
+    }
+        
   }
   
   var player = new Ball();
   
   console.log(player.x);
   console.log(player.y);
-  player.draw(50, 300);
+  
+  
+  var Game = setInterval( function () {
+    player.draw(player.x, player.y);
+  }, 30);
 });
        
