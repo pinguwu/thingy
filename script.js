d9312f4d1ffe603b95ff7d36dfe7b904ee606747
pinguwu <42652753+pinguwu@users.noreply.github.com>
2019-09-17 13:34:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready( function () {
   // canvas
-  var canvas = document.getElementById("canvas");
+  var canvas = document.getElementById("theCanvas");
   var ctx = canvas.getContext("2d");
   
   // jumper?
